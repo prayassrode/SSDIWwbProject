ef36b1cdb02492c808c7f95619e85989dfba0957
prayassrode <prayassrode@gmail.com>
2021-02-12 10:28:53 -0500

    Initial commit

---
+++ src/test/java/com/test/TestClass.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by praya on 2/24/2017.
+ */
+public class TestClass {
+
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
